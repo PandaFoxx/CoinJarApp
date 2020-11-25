5d617bd870ba6b37442126e68a9e7f5fab0f1899
Kyle Champion <kyleischampion@gmail.com>
2020-11-25 22:15:28 +0200

    Added a reference to "coins in circulation" in the business rules.

--- Business/Rules.cs
+++ Business/Rules.cs
@@ -5,6 +5,8 @@ namespace CoinJarApp.Business
   public class Rules : IRules
   {
     private const decimal MaxVolume = 42;
+
+    // According to https://en.wikipedia.org/wiki/Coins_of_the_United_States_dollar
     private readonly decimal[] _coinsInCirculation = {0.01m, 0.05m, 0.1m, 0.25m, 0.5m, 1.0m};
 
     public decimal GetCapacity()
